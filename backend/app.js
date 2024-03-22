63004d33ffea3ea44a967ec040104daeaa259355
stevegodly <stevegodly12@gmail.com>
2024-03-22 10:00:23 +0530

    cors update

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app=express()
 
 app.use(express.json())
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'https://book-store-delta-mauve.vercel.app',
     methods:['GET','POST','PATCH','DELETE'],
     allowedHeaders:['Content-Type'],    
 }))
